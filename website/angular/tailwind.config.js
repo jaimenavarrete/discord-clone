d9a6eaab7f6a5204b5a726afd869d734419fbece
Jaime Navarrete <jaimenava123@outlook.com>
2023-09-05 19:20:31 -0600

    Add discord color palette in tailwind config file

--- website/angular/tailwind.config.js
+++ website/angular/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#F2F3F5',
+      primary: '#5865F2',
+      secondary: '#23A559',
+      black: {
+        100: '#313338',
+        200: '#2B2D31',
+        300: '#232428',
+        400: '#1E1F22',
+      }
+    }
   },
   plugins: [],
 }
